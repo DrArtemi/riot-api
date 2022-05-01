e39e71db5a0e876e256d67dfeedfadc84d382be0
Adrien Milcent <adrien.milcent@outlook.com>
2022-05-01 11:10:17 +0200

    Add sidebar and filters for matches (#12)
    * add sidebar and filters for matches
    
    * add filters to every view and style them

--- lol-esport-stats/src/components/MatchList.svelte
+++ lol-esport-stats/src/components/MatchList.svelte
@@ -2,11 +2,15 @@
     import * as moment from 'moment';
 
     export let matches;
+    export let tournaments_filters;
+    export let stages_filters;
 </script>
 
-<table class="text-left border-collapse w-full">
+<table class="text-left border-collapse w-full bg-slate-900">
     <thead>
         <tr>
+            <th class="sticky z-10 top-0 p-0 bg-slate-900 text-slate-300"><div class="py-2 pl-4 pr-2 border-b border-amber-400/40">Tournament</div></th>
+            <th class="sticky z-10 top-0 p-0 bg-slate-900 text-slate-300"><div class="py-2 pr-2 border-b border-amber-400/40">Stage</div></th>
             <th class="sticky z-10 top-0 p-0 bg-slate-900 text-slate-300"><div class="py-2 pr-2 border-b border-amber-400/40">Date</div></th>
             <th class="sticky z-10 top-0 p-0 bg-slate-900 text-center text-slate-300"><div class="py-2 pr-2 border-b border-amber-400/40">Blue side</div></th>
             <th class="sticky z-10 top-0 p-0 bg-slate-900 text-center text-slate-300"><div class="py-2 pr-2 border-b border-amber-400/40">Red side</div></th>
@@ -15,16 +19,20 @@
     </thead>
     <tbody>
         {#each matches as match (match.id)}
+        {#if tournaments_filters.includes(match.stage.tournament.slug) && stages_filters.includes(match.stage.id)}
         <tr>
+            <td class="w-1 whitespace-nowrap text-slate-200 capitalize py-2 pl-4 pr-4 border-t border-slate-400/10">{match.stage.tournament.slug.replaceAll('_', ' ')}</td>
+            <td class="w-1 whitespace-nowrap text-slate-200 py-2 pr-4 border-t border-slate-400/10">{match.stage.name}</td>
             <td class="text-slate-200 py-2 pr-2 border-t border-slate-400/10">{moment(match.date).format('DD-MMM-YYYY')}</td>
-            <td class="py-2 pr-2 border-t border-slate-400/10"><img class="mx-auto h-12 w-12 p-1 rounded-full" class:bg-sky-200="{match.team_1_win}" class:bg-red-200="{!match.team_1_win}" src="{match.team_1.image}" alt="{match.team_1.name}"></td>
-            <td class="py-2 pr-2 border-t border-slate-400/10"><img class="mx-auto h-12 w-12 p-1 rounded-full" class:bg-sky-200="{match.team_2_win}" class:bg-red-200="{!match.team_2_win}" src="{match.team_2.image}" alt="{match.team_2.name}"></td>
+            <td class="py-2 pr-2 border-t border-slate-400/10"><img class="mx-auto h-12 w-12 p-1 rounded-full" class:bg-sky-300="{match.team_1_win}" class:bg-red-300="{!match.team_1_win}" src="{match.team_1.image}" alt="{match.team_1.name}"></td>
+            <td class="py-2 pr-2 border-t border-slate-400/10"><img class="mx-auto h-12 w-12 p-1 rounded-full" class:bg-sky-300="{match.team_2_win}" class:bg-red-300="{!match.team_2_win}" src="{match.team_2.image}" alt="{match.team_2.name}"></td>
             <td class="text-sm text-slate-200 py-2 pr-2 border-t border-slate-400/10">
                 <a href="https://feed.lolesports.com/livestats/v1/details/{parseInt(match.riot_id) + 1}" target="_blank">
                     <svg class="mx-auto" width="24" height="24" viewBox="-3 -3 30 30" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path fill="currentColor" d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z"/></svg>
                 </a>
             </td>
         </tr>
+        {/if}
         {/each}
     </tbody>
 </table>
--- lol-esport-stats/src/routes/leagues.svelte
+++ lol-esport-stats/src/routes/leagues.svelte
@@ -1,23 +1,82 @@
 <script>
-    import MatchList from '../components/MatchList.svelte';
     import { page } from '$app/stores';
     import { query } from 'svelte-apollo';
-    import { LEAGUE_MATCHES } from '../components/queries';
+    import { LEAGUE_MATCHES, TOURNAMENTS_BY_SLUGS } from '../components/queries';
+    import SideBar from '../components/SideBar.svelte';
+    import MatchList from '../components/MatchList.svelte';
+    import MatchFilters from '../components/MatchFilters.svelte';
 
     const slug = $page.query.get('slug');
+    let tournament_slugs = [];
+    let tournaments_stages = {};
+    let tournaments_filters = [];
+    let stages_filters = [];
 
     const matches = query(LEAGUE_MATCHES, {
         variables: { "league": slug }
     });
+    $: matches.refetch({ "league": slug });
+    matches.subscribe(result => {
+        if (result.data) {
+            tournament_slugs = [];
+            for (const match of result.data.leagueMatches)
+                if (!tournament_slugs.includes(match.stage.tournament.slug))
+                    tournament_slugs.push(match.stage.tournament.slug);
+        }
+    });
+    const tournaments = query(TOURNAMENTS_BY_SLUGS, {
+        variables: { "slugs": tournament_slugs }
+    });
+    $: tournaments.refetch({ "slugs": tournament_slugs });
+    tournaments.subscribe(result => {
+        if (result.data) {
+            for (const tournament of result.data.tournamentsBySlugs) {
+                tournaments_filters.push(tournament.slug);
+                tournaments_stages[tournament.slug] = [];
+                for (const stage of tournament.stages) {
+                    stages_filters.push(stage.id);
+                    tournaments_stages[tournament.slug].push(stage.id);
+                }
+            }
+            tournaments_filters = [...tournaments_filters]; // C'est quand même fou de devoir faire ça fuck le js
+            stages_filters = [...stages_filters]; // C'est quand même fou de devoir faire ça fuck le js
+        }
+    });
+
+    const tabs = ["matches", "statistics"];
+    let activeTab = 0;
 </script>
 
-<div class="mt-32 px-8">
-    <h1 class="text-4xl mb-8 capitalize text-slate-400">{slug}</h1>
-    <div class="content text-slate-200 max-h-96 w-1/3 overflow-auto">
-    {#if $matches.data}
-        <MatchList matches={$matches.data.leagueMatches} />
-    {:else}
-        <p>No match found</p>
-    {/if}
+<div class="pt-24 pb-2 px-4 h-full w-full flex flex-col">
+    <div class="flex-initial w-full bg-slate-800/30 border-l-2 border-l-amber-400">
+        <MatchFilters
+            tournaments={tournaments}
+            tournaments_stages={tournaments_stages}
+            bind:tournaments_filters={tournaments_filters}
+            bind:stages_filters={stages_filters}
+        />
+    </div>
+    <div class="flex flex-row flex-1 min-h-0 mt-2 border-l-2 border-l-amber-400">
+        <div class="w-64 bg-slate-800/30 flex-initial">
+            <SideBar title={slug} tabs={tabs} bind:activeTab={activeTab} />
+        </div>
+        <div class="w-full h-full ml-2 p-8 bg-slate-800/30 flex flex-col flex-1">
+            {#if tabs[activeTab] == "matches"}
+            <h2 class="text-slate-300 text-3xl mb-8 flex-initial">Match list</h2>
+            <div class="text-slate-200 overflow-auto flex-1">
+                {#if $matches.data}
+                <MatchList
+                    tournaments_filters={tournaments_filters}
+                    stages_filters={stages_filters}
+                    matches={$matches.data.leagueMatches}
+                />
+                {:else}
+                <p>No match found</p>
+                {/if}
+            </div>
+            {:else if tabs[activeTab] == "statistics"}
+            <p class="text-red-600">STATS</p>
+            {/if}
+        </div>
     </div>
 </div>
